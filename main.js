d348d0a4dbdaf3895640587377ee1320d8c7800e
Fernandakruk <128099736+Fernandakruk@users.noreply.github.com>
2024-05-24 12:17:42 -0300

    Segunda aula
    socorro

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+{
+ enunciado: "Inseira o enunciado da Pergunta 1",
+ alternativas: ["Alternativas 1", "Alternativas 2"]
+},
+{
+    enunciado: "Insira o enunciado da Pergunta 2",
+    alternativas: ["Alternativa 1","Alternativa 2" ]
+},
+{
+    enunciado: "Inisra o enunciado da Pergunta 3",
+    alternativas: ["Alternativas 1", "Alternativas 2"]
+}
+];
