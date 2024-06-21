a5d25ef6cb89269307af7688c0d3ff3922b64425
Fernandakruk <128099736+Fernandakruk@users.noreply.github.com>
2024-06-21 12:25:21 -0300

    a2

--- main.js
+++ main.js
@@ -25,21 +25,39 @@ const perguntas = [
         "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.", 
         "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
     ]
-}
-],
+},
 {
     enunciado:"Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
     alternativas: [
         "Criar uma imagem utilizando uma plataforma de design como o Paint.",
         "Criar uma imagem utilizando um gerador de imagem de IA."
     ]
-}
-[,
+},
 {
+
     enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
     alternativas: [
         "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
         "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
     ]
+},
+];
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
-]
+function mostraPergunta() {
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    
+
+    }
+}
+
+mostraPergunta();
